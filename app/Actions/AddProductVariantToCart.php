c4f29b0bca89c5604d04879d5f4fed21d0603a84
jaiveer-truptman <jchavda@truptman.in>
2025-06-12 06:19:22 +0530

    persist cart item

--- app/Actions/AddProductVariantToCart.php
+++ app/Actions/AddProductVariantToCart.php
@@ -8,17 +8,16 @@ class AddProductVariantToCart
 {
     public function add($variantId)
     {
-        if (auth()->guest()) {
-            $cart = Cart::query()->firstOrCreate([
-                'session_id' => session()->getId(),
-            ]);
-        }
+        $cart = match (auth()->guest()) {
+            true => Cart::query()->firstOrCreate(['session_id' => session()->getId()]),
+            false => auth()->user()->cart ?: auth()->user()->cart()->create()
+        };
 
-        if (auth()->user()) {
-            $cart = auth()->user()->cart ?: auth()->user()->cart()->create();
-        }
+        $temp = $cart->items()->create([
+            'product_variant_id' => $variantId,
+            'quantity' => 1,
+        ]);
 
-        dd($cart);
-
+        
     }
 }
