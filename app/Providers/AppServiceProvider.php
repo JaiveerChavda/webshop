7a2f09e11be2282cc42c4c0f1ae9d3fa4cc6e463
JaiveerChavda <jayveersinhchavda555@gmail.com>
2025-06-28 14:57:39 +0530

    install and set up stripe cashier

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Cashier\Cashier;
 use Money\Currencies\ISOCurrencies;
 use Money\Formatter\IntlMoneyFormatter;
 use Money\Money;
