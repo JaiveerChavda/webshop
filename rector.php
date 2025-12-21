03c3803d4230b682a9df169f15c98bf9d44b8dc3
JaiveerChavda <jayveersinhchavda555@gmail.com>
2025-12-21 14:32:29 +0530

    add new rector-pest package

---
+++ rector.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use RectorPest\Set\PestSetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/app',
+        __DIR__ . '/bootstrap',
+        __DIR__ . '/config',
+        __DIR__ . '/public',
+        __DIR__ . '/resources',
+        __DIR__ . '/routes',
+        __DIR__ . '/tests',
+    ])->withSets([
+        PestSetList::PEST_CODE_QUALITY
+    ]);    
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    // ->withTypeCoverageLevel(0)
+    // ->withDeadCodeLevel(0)
+    // ->withCodeQualityLevel(0);
