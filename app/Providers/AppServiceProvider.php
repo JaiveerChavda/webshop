7fe75e6252e90962362dd8def94e0ce57e5b09a1
JaiveerChavda <jayveersinhchavda555@gmail.com>
2026-01-24 21:39:56 +0530

    install laravel telescope & configure it

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(StripeGateway::class,CashierStripeGateway::class);
+
+        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
