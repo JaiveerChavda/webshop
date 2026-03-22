d0018343a73d34a559834482913adc1be03a57b6
JaiveerChavda <jayveersinhchavda555@gmail.com>
2026-03-22 22:10:29 +0530

    add wire:Navigate to cart route

--- resources/views/livewire/navigation-cart.blade.php
+++ resources/views/livewire/navigation-cart.blade.php
@@ -1,5 +1,5 @@
 <div>
     <flux:navbar class="me-1.5 space-x-0.5 rtl:space-x-reverse py-0!">
-        <flux:navbar.item href="{{ route('cart') }}" icon="shopping-cart" :badge="$this->count">Cart</flux:navbar.item>
+        <flux:navbar.item href="{{ route('cart') }}" wire:navigate icon="shopping-cart" :badge="$this->count">Cart</flux:navbar.item>
     </flux:navbar>
 </div>
