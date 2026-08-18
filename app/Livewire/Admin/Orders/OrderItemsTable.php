a50d9e1541a9417b96df40b579bc955407a9d223
JaiveerChavda <mrjaiveer.dev@gmail.com>
2026-08-18 23:39:22 +0530

    feat: show total of order itmes amount by using filament summaries

--- app/Livewire/Admin/Orders/OrderItemsTable.php
+++ app/Livewire/Admin/Orders/OrderItemsTable.php
@@ -8,11 +8,13 @@ use Filament\Actions\Concerns\InteractsWithActions;
 use Filament\Actions\Contracts\HasActions;
 use Filament\Schemas\Concerns\InteractsWithSchemas;
 use Filament\Schemas\Contracts\HasSchemas;
+use Filament\Tables\Columns\Summarizers\Sum;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
 use Livewire\Component;
+use Money\Currency;
 use Money\Money;
 
 class OrderItemsTable extends Component implements HasActions, HasSchemas, HasTable
@@ -33,11 +35,25 @@ class OrderItemsTable extends Component implements HasActions, HasSchemas, HasTa
 
                 TextColumn::make('description'),
 
+                TextColumn::make('price')
+                    ->formatStateUsing(fn (Money $state) => MoneyFormatter::format($state)),
+
                 TextColumn::make('quantity')
                     ->alignCenter(),
 
-                TextColumn::make('price')
-                    ->formatStateUsing(fn (Money $state) => MoneyFormatter::format($state)),
+                TextColumn::make('amount_total')
+                ->label('Total')
+                    ->formatStateUsing(fn (Money $state) => MoneyFormatter::format($state))
+                    ->summarize(
+                        Sum::make()
+                            ->label('Total')
+                            ->formatStateUsing(
+                                fn ($state) => MoneyFormatter::format(
+                                    new Money($state, new Currency('USD'))
+                                )
+                            )
+                    ),
+
             ]);
     }
 
